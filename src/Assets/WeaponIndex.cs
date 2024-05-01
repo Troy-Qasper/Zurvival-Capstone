6ae4265fe3f70203421469fddf069a3280e6b829
Troy Snook-Purvis <81993270+Troy-Qasper@users.noreply.github.com>
2024-04-30 23:31:04 -0500

    Deleting currently unused scripts

--- src/Assets/WeaponIndex.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class WeaponIndex : MonoBehaviour
-{
-    public int setWeapon = 1;
-    public int setPrice = 10;
-}
